2c671d5625e7d1a29a3674b6d49f4bbde319b66f
Daniel Løvbrøtte Olsen <daniel.olsen99@gmail.com>
2015-12-15 10:08:55 +0100

    Added Global.css

--- home/index.php
+++ home/index.php
@@ -7,6 +7,10 @@
 <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.indigo-pink.min.css">
 <script src="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" type="text/css" href="../global.css">
+
+
+
 
 <!-- Sidebar -->
 <!-- No header, and the drawer stays open on larger screens (fixed drawer). -->
