939a56974167ab410aff7c7694489e9cc2cd37c3
Daniel Løvbrøtte Olsen <daniel.olsen99@gmail.com>
2016-02-03 11:42:40 +0100

    Added default navigation

---
+++ User/index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+	<title>[TITLE]</title>
+</head>
+<body>
+
+<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.indigo-pink.min.css">
+<script src="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" type="text/css" href="../global.css">
+
+
+
+
+<!-- Sidebar -->
+<!-- No header, and the drawer stays open on larger screens (fixed drawer). -->
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer vertical-center">
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">[Title]</span>
+    <nav class="mdl-navigation">
+      <a class="mdl-navigation__link" href="">Posts</a>
+      <a class="mdl-navigation__link" href="">Comments</a>
+      <a class="mdl-navigation__link" href="">Tags</a>
+      <a class="mdl-navigation__link" href="">Wiki</a>
+      <a class="mdl-navigation__link" href="">Documentation</a>
+    </nav>
+  </div>
+  <main class="mdl-layout__content">
+    <div class="page-content">
+	
+
+
+
+
+
+</div>
+</main>
+</div>
+</body>
